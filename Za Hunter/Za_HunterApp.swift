6aa14ed1975a9cce3c719613a23432d3f901bbb1
Quinn Zona <qzona@bsd220.org>
2022-03-03 10:24:14 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Student on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
